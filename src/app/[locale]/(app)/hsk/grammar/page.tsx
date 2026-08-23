800ff9c0b765f1ae185b2baaebf30c4b3280944b
lekhanh2099 <69201028+lekhanh2099@users.noreply.github.com>
2026-08-23 21:21:37 +0700

    revert(hsk): remove grammar prototype route

--- src/app/[locale]/(app)/hsk/grammar/page.tsx
+++
@@ -1,9 +0,0 @@
-import { HskGrammarLibrary } from "@/features/hanzihome/hsk-grammar/HskGrammarLibrary";
-
-export default function HskGrammarPage() {
- return (
-  <div className="hanzihome-static-page min-w-0 p-3 sm:p-5 lg:p-6">
-   <HskGrammarLibrary />
-  </div>
- );
-}
