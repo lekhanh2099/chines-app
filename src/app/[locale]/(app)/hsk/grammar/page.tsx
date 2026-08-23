f25a9bb26052d1762921714a16a5e09e45ef78d2
lekhanh2099 <69201028+lekhanh2099@users.noreply.github.com>
2026-08-23 18:50:37 +0700

    feat(hsk): mount grammar under HSK route

---
+++ src/app/[locale]/(app)/hsk/grammar/page.tsx
@@ -0,0 +1,9 @@
+import { HskGrammarLibrary } from "@/features/hanzihome/hsk-grammar/HskGrammarLibrary";
+
+export default function HskGrammarPage() {
+ return (
+  <div className="hanzihome-static-page min-w-0 p-3 sm:p-5 lg:p-6">
+   <HskGrammarLibrary />
+  </div>
+ );
+}
